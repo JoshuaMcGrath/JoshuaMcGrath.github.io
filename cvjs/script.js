c763298062910a7a73e878e54e881db5dd2628ca
joshuamcgrath <jmgcronin@gmail.com>
2019-12-09 19:46:46 +0000

    Edited scroll

--- cvjs/script.js
+++ cvjs/script.js
@@ -1,19 +1,16 @@
 (function($) {
-  "use strict"; // Start of use strict
+  "use strict"; 
 
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.scroll').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#navbarTop',
     offset: 100
   });
 
-  // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#navbarTop").offset().top > 100) {
       $("#navbarTop").addClass("navbar-shrink");
@@ -21,39 +18,37 @@
       $("#navbarTop").removeClass("navbar-shrink");
     }
   };
-  // Collapse now if page is not at top
   navbarCollapse();
-  // Collapse the navbar when page is scrolled
+
   $(window).scroll(navbarCollapse);
 
 
 
 
-})(jQuery); // End of use strict
+})(jQuery); 
 
 
 $(document).ready(function () {
-  // Add smooth scrolling to all links
-  $("a").on('click', function (event) {
+  
+  $(".scroll").on('click', function (event) {
 
-    // Make sure this.hash has a value before overriding default behavior
+   
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
+
       event.preventDefault();
 
-      // Store hash
+    
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+   
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function () {
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
+       
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
 
 });
